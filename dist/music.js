2b7ffc2696a46af25f253f8a009e8c9e3a5d5344
heheql <374655767@qq.com>
2018-12-08 01:49:49 +0800

    Site updated: 2018-12-08 01:49:48

--- dist/music.js
+++ dist/music.js
@@ -4,14 +4,14 @@ const ap = new APlayer({
     autoplay: false,
     audio: [
       {
-        name: "PDD洪荒之力",
-        artist: '徐梦圆',
+        name: "未闻花名",
+        artist: '未闻花名',
         url: 'http://www.ytmp3.cn/down/48499.mp3',
       },
       {
-        name: '9420',
-        artist: '麦小兜',
-        url: 'http://up.mcyt.net/?down/45967.mp3',
+        name: 'next to you',
+        artist: '',
+        url: 'http://www.ytmp3.cn/down/33709.mp3',
       },
     ]
 });
