ad3ca22f35dd67ae1c8eefc55b88785175b2d6a7
heheql <374655767@qq.com>
2018-12-08 01:46:34 +0800

    Site updated: 2018-12-08 01:46:32

---
+++ dist/music.js
@@ -0,0 +1,17 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: "PDD洪荒之力",
+        artist: '徐梦圆',
+        url: 'http://www.ytmp3.cn/down/48499.mp3',
+      },
+      {
+        name: '9420',
+        artist: '麦小兜',
+        url: 'http://up.mcyt.net/?down/45967.mp3',
+      },
+    ]
+});
